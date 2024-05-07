c9137c41da14bbb6d5db78dcc8c6707e407ef0f2
nayon117 <hasibul.nayon1@gmail.com>
2024-05-07 23:25:21 +0600

    fix response issue

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,28 +1,27 @@
-
 import { EmailTemplate } from "../../../components/shared/EmailTemplate";
 import { Resend } from "resend";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    const body = await req.body;
-    console.log(body)
+  const body = await req.body;
+  console.log(body);
 
-    try {
-      const data = await resend.emails.send({
-        from: "Acme <onboarding@resend.dev>",
-        to: ["fromdevtoprod23@gmail.com"],
-        subject: "Hello world",
-        react: EmailTemplate({
-          firstName: body.firstName || "empty",
-          lastName: body.lastName || "empty",
-          email: body.email || "empty",
-          message: body.message || "empty",
-        }),
-      });
-      return Response.json(data);
-    } catch (error) {
-      return Response.json({ error });
-    }
+  try {
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["fromdevtoprod23@gmail.com"],
+      subject: "Hello world",
+      react: EmailTemplate({
+        firstName: body.firstName || "empty",
+        lastName: body.lastName || "empty",
+        email: body.email || "empty",
+        message: body.message || "empty",
+      }),
+    });
+    return res.json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
